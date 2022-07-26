f4fb83b64564e656b9c314ed4e53dd96d5c28d07
Divyanshu2910 <109759828+Divyanshu2910@users.noreply.github.com>
2022-07-26 12:47:33 +0530

    Initial commit

---
+++ app/src/main/java/com/Studio/Studiologin/ForgotPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.Studio.Studiologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forgot_password_main)
+    }
+}
---
+++ app/src/main/java/com/Studio/Studiologin/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.Studio.Studiologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class
+MainActivity : AppCompatActivity() {
+    lateinit var etEmail:EditText
+    lateinit var etPassword :EditText
+    lateinit var tvforgotpassword: TextView
+    lateinit var btnLogin: Button
+    lateinit var tvSignup: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+        tvforgotpassword=findViewById(R.id.tvforgotpassword)
+        btnLogin=findViewById(R.id.btnLogin)
+        tvSignup=findViewById(R.id.tvSignup)
+        btnLogin.setOnClickListener{
+            System.out.println("clicked")
+            var email = etEmail.text.toString()
+            var password = etPassword.text.toString()
+            System.out.println("email $email")
+            if (email.isNullOrEmpty())
+            {
+                etEmail.error = resources.getString(R.string.please_enter_email)
+                etEmail.requestFocus()
+            }
+            else if (password.isNullOrEmpty())
+            {
+                etPassword.error = resources.getString(R.string.please_enter_Password)
+                etPassword.requestFocus()
+            }
+            else
+            {
+                Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
+
+
+    }
+}
